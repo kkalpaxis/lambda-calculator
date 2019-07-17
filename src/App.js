15e2a70ce21620ffb73f23ba7191c6f7a7f2de45
Karina Kalpaxis <kalpaxis1@gmail.com>
2019-07-17 17:20:27 -0400

    After Hours Progress

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import Numbers from './components/ButtonComponents/NumberButtons/Numbers';
 import Operators from './components/ButtonComponents/OperatorButtons/Operators';
 import Specials from './components/ButtonComponents/SpecialButtons/Specials';
+import Display from './components/DisplayComponents/Display';
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
 
@@ -20,9 +21,10 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-        <Numbers/>
-        <Operators/>
-        <Specials/>
+        <Display number={1024} />
+        <Numbers />
+        <Operators />
+        <Specials />
       </div>
     </div>
   );
